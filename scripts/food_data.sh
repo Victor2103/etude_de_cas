0b061f38c7735358af18aeda51b8fac33c834f14
a-leksi <aleksi.dufour@gmail.com>
2023-04-28 20:37:13 +0200

    make dataset

--- scripts/food_data.sh
+++ scripts/food_data.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-mkdir data1 && cd data1
+mkdir data || cd data
 wget http://data.vision.ee.ethz.ch/cvl/food-101.tar.gz
 tar -xf food-101.tar.gz
 rm food-101.tar.gz
--- source/make_food.py
+++ source/make_food.py
@@ -6,17 +6,14 @@ import tarfile
 
 url = "http://data.vision.ee.ethz.ch/cvl/food-101.tar.gz"
 path = 'data/food.tar.gz'
- 
+
 try:
     os.mkdir("data")
-    req = requests.get(url, stream=True)
-    if req.status_code == 200:
-        with open(path, 'wb') as f:
-            f.write(req.raw.read())
-    tar  = tarfile.open(path)
-    tar.extractall('data')
-    tar.close
+except Exception as e:
+    print(e)
 
+try:
+    os.system('bash scripts/food_data.sh')
 except Exception as e:
     print(e)
 
